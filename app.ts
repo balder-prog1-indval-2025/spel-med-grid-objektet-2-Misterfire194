837c3fcb4f2b98d85ee22077f15fa2609904d081
Elliot <ebu0729@student.skelleftea.se>
2025-09-19 08:37:42 +0200

    Fördjupning fortf. ej klar

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
-let g = new Grid(10, 10)
+let g = new Grid(20, 20)
 
 let player_o = true
 update = () => {
     if (g.activated) {
         g.activeCell.custom = (c) => {
             if (player_o && c.tag != "x" || c.tag == "o") {
-                circle(c.x + c.width/2, c.y + c.height/2, c.height/2 - 20, "blue", 5)
+                circle(c.x + c.width/2, c.y + c.height/2, c.height/2, "blue", 5)
                 c.tag = "o"
             } else {
-                line(c.x + 20, c.y + 20, c.x + c.width - 20, c.y + c.height - 20, "red", 5)
-                line(c.x + c.width - 20, c.y + 20, c.x + 20, c.y + c.height - 20, "red", 5)
+                line(c.x, c.y, c.x + c.width, c.y + c.height, "red", 5)
+                line(c.x + c.width, c.y, c.x, c.y + c.height, "red", 5)
                 c.tag = "x"
             }
             if (c.tag == "x") {
